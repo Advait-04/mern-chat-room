6b02195aa5b2ac54107f44c0e74e25191ac9660e
Advait Suresh <103671162+Advait-04@users.noreply.github.com>
2023-08-01 20:08:35 +0530

    Update useSignup.js for deployment

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
         setIsLoading(true);
         setError(null);
 
-        const response = await fetch("/api/signup", {
+        const response = await fetch("https://mern-chat-room-backend.vercel.app/api/signup", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password }),
