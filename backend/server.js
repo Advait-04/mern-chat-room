d87773a0f08616f708538c9ee7e4d24a6cf8091f
Advait Suresh <103671162+Advait-04@users.noreply.github.com>
2023-08-01 21:23:16 +0530

    Update server.js cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 
 app.use(
     cors({
-        origin: "https://mern-chat-room.vercel.app/"
+        origin: "https://mern-chat-room.vercel.app"
     })
 );
 
