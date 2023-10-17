f9a92b0cb851964512e6f52e15b89e8452fed734
Advait Suresh <103671162+Advait-04@users.noreply.github.com>
2023-10-17 18:30:23 +0530

    Update SignUp.js fix

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useSignup } from "../hooks/useSignup";
